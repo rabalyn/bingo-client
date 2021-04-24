0a2d8cb6920b1f88562bdeb65cf5ce5bd6c15bbc
nm94cecu <nils.mundhenke@tu-darmstadt.de>
2021-04-24 19:42:26 +0200

    add ScrollToTop

---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,41 @@
+<template>
+  <a
+    v-show="visible"
+    class="bottom-right"
+    @click="scrollTop"
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: false
+    }
+  },
+  mounted: function () {
+    window.addEventListener('scroll', this.scrollListener)
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('scroll', this.scrollListener)
+  },
+  methods: {
+    scrollTop: function () {
+      this.intervalId = setInterval(() => {
+        if (window.pageYOffset === 0) {
+          clearInterval(this.intervalId)
+        }
+        window.scroll(0, window.pageYOffset - 50)
+      }, 20)
+    },
+    scrollListener: function (e) {
+      this.visible = window.scrollY > 150
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
